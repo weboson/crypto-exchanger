c3c140cc04d5aaa541c71073b41b0cfac7a8abc4
Rishat <42917939+weboson@users.noreply.github.com>
2025-06-05 22:25:19 +0500

    Added assets files, created page: dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  '../assets/css/soft-ui-dashboard.css'
+
+const page = () => {
+    return (
+<>
+<div className='text-danger'>Hello</div>
+<h6 className="font-weight-bolder mb-0">Billing</h6>
+</>
+    );
+};
+
+export default page;
